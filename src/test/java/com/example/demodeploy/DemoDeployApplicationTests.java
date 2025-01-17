a163d0d49aa6f0b9bbd654ce2caa9a05e0ad3b08
Shining17 <bbvv777@naver.com>
2025-01-17 14:45:29 +0900

    Feat: gitaction 단위테스트 오류 발생

--- src/test/java/com/example/demodeploy/DemoDeployApplicationTests.java
+++ src/test/java/com/example/demodeploy/DemoDeployApplicationTests.java
@@ -10,9 +10,13 @@ class DemoDeployApplicationTests {
     void contextLoads() {
         // 단위 테스트
         // MVC 테스트 -> 목업통해서 가상 처리 가능
+//        테스트가 실패 되면 반영 x
+        int a= 1/0;
+        throw new RuntimeException("오류발생");
+
         // RestAPI 테스트 가능
         // 메세지가 보이지는 않지만 , 차후 코드로 확인
-        System.out.println("단위 테스트 호출!! echo");
+//        System.out.println("단위 테스트 호출!! echo");
     }
 
 }
